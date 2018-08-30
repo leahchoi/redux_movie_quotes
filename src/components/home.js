ede79f2178b66844e65ade5e22ee3b2220f1c261
leahchoi <soora913@gmail.com>
2018-08-29 17:58:56 -0700

    setup the webpage

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Welcome to Movie Quotes</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam illum molestiae, eos facilis doloribus aperiam voluptate repellendus, suscipit dignissimos sed dolorem repudiandae!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam illum molestiae, eos facilis doloribus aperiam voluptate repellendus, suscipit dignissimos sed dolorem repudiandae!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam illum molestiae, eos facilis doloribus aperiam voluptate repellendus, suscipit dignissimos sed dolorem repudiandae!</p>
+    </div>
+)
---
+++ src/components/secret_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">List of Operatives</h1>
+        <ol>
+            <li>Tom</li>
+            <li>Amy</li>
+            <li>Julio</li>
+            <li>Ashley</li>
+            <li>Mary Kate</li>
+        </ol>
+    </div>
+)
