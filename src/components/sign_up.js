5d406cb6d5c38cf1048dc066684bce8763ff082a
leahchoi <soora913@gmail.com>
2018-08-31 15:03:12 -0700

    completed auth HOC

--- src/components/sign_up.js
+++ src/components/sign_up.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class SignUp extends Component {
     render(){
+        console.log('signup proprs;', this.props)
         return (
             <div>
                 <h1 className="center">Sign Up</h1>
---
+++ src/hoc/auth.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export default function(WrappedComponent, path){
+    class Auth extends Component {
+        componentDidMount(){
+            this.checkAuth();
+        }
+        componentDidUpdate(){
+            const { auth, history } = this.props;
+
+            if (!auth) {
+                history.push('/');
+            }
+        }
+        checkAuth(){
+            const { auth, history } = this.props;
+
+            if (!auth) {
+                history.push('/');
+            }
+        }
+        render(){
+            return <WrappedComponent {...this.props}/>
+        }
+    }
+    function mapStateToProps(state){
+        return{
+            auth: state.user.auth
+        }
+    }
+    return connect(mapStateToProps)(Auth);
+}
