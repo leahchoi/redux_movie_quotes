b7005e9b00dac19e5d5238d74bd4244c3187d8dd
leahchoi <soora913@gmail.com>
2018-08-31 11:45:34 -0700

    redux setup complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import App from './components/app';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers';
 import 'materialize-css/dist/css/materialize.min.css'
 
+const store = createStore(rootReducer, {}, applyMiddleware());
+
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
